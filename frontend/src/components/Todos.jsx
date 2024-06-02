43b895180a56f067f6457c911df88b795c0032a5
sept1st2c <sept1st2c@gmail.com>
2024-06-02 21:03:03 +0530

    frontend structure

---
+++ frontend/src/components/Todos.jsx
@@ -0,0 +1,9 @@
+export function Todos () {
+        return (
+                <div>
+                        <h1>Go to Gym</h1>
+                        <h1>Go to Gym at 5.30pm</h1>
+                        <button>Mark as done</button>
+                </div>
+        )
+}
